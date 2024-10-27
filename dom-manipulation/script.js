f4ae04b0326ddf9bd254cd31357cca9c7ba91f22
James Moses <jamxsmosxs@gmail.com>
2024-10-27 11:48:18 +0100

    Initial commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,4 +1,4 @@
-const quotes = [
+const text = [
   {
     quote: `I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.`,
     category: "Marilyn Monroe",
