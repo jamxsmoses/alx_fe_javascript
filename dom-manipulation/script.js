7cfb1ca1045e2cc39cb5cad4ee1ac26532716736
James Moses <jamxsmosxs@gmail.com>
2024-10-27 11:34:27 +0100

    Initial commit

---
+++ dom-manipulation/script.js
@@ -0,0 +1,24 @@
+const quotes = [];
+const displayQuote = document.getElementById("quoteDisplay");
+const btn = document.getElementById("newQuote");
+const newQuoteText = document.getElementById("newQuoteText");
+const newQuoteCategory = document.getElementById("newQuoteCategory");
+
+function showRandomQuote() {
+  const randomNum = Math.floor(Math.random() * quotes.length);
+  displayQuote.textContent = quotes[randomNum].quote;
+}
+
+btn.addEventListener("click", showRandomQuote);
+
+function addQuote() {
+  const quoteDetails = {};
+
+  if (!newQuoteText && !newQuoteCategory) return;
+
+  quoteDetails.quote = newQuoteText.value;
+  quoteDetails.category = newQuoteCategory.value;
+
+  quotes.push(quoteDetails);
+  console.log(quotes);
+}
